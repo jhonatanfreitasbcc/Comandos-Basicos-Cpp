11f0934c7db84aa97f310637b447a1fb02c413be
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-12 11:45:33 -0300

    Como Somar 2 Números

---
+++ Como Somar 2 Números.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int numero;
+    int numero2;
+    int resultado;
+    
+    cout << "Digite o primeiro numero: ";
+    cin >> numero;
+    
+    cout << "Digite o segundo numero: ";
+    cin >> numero2;
+    
+    resultado = numero + numero2;
+    
+    cout << "O resultado da soma dos numeros são: " << resultado << endl;
+    
+    return 0;
+}
