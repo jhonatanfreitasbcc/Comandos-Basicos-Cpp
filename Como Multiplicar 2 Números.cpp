ef0f79cae5781ef1ca460dd5d10f1cc4efda5f25
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-12 12:00:14 -0300

    Como Multiplicar 2 Números

---
+++ Como Multiplicar 2 Números.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int numero;
+    int numero2;
+    int mult;
+    
+    cout << "Digite um numero: ";
+    cin >> numero;
+    
+    cout << "Digite o segundo numero: ";
+    cin >> numero2;
+    
+    mult = numero * numero2;
+    
+    cout << "A multiplicação dos numeros são: " << mult << endl;
+    
+    return 0;
+}
