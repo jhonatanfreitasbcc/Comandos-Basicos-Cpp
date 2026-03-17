34604ae0599180361ec8bfb6fcae205ba43d9db3
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-17 09:14:32 -0300

    Exemplo de Comando While

---
+++ Exemplo de Comando While.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int n = 0;
+    
+    while(n < 10){
+        cout << n << endl;
+        n++;
+    }
+    
+    cout << "programa finalizado" << endl;
+    
+    return 0;
+}
