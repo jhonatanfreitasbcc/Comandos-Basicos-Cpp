0eb346197a79d14c3f8ffbd5f583ebd7bd8d2c19
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-17 11:28:16 -0300

    Imprimir Strings Sequenciais com Laço Do While

---
+++ Imprimir Strings Sequenciais com Laço Do While.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int contador = 0;
+    
+    do{
+        cout << "inscrevam se no meu canal" << endl;
+        contador++;
+    }while(contador < 20);
+    
+    cout << "Programa finalizado" << endl;
+    
+    return 0;
+}
