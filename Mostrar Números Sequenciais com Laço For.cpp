b2b13e1b44374489141d0964dc6a4ffd323511e7
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-17 13:07:45 -0300

    Mostrar Números Sequenciais com Laço For

---
+++ Mostrar Números Sequenciais com Laço For.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int valor;
+    
+    for (valor = 0; valor < 20; valor++){
+        cout << valor << endl;
+    }
+    cout << "Programa Finalizado" << endl;
+    
+    return 0;
+}
