c64ff11603a9cd8430dd33389fead800e2f49e54
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-17 10:54:56 -0300

    Imprimir Números Pares com Laço While

---
+++ Imprimir Números Pares com Laço While.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int contador = 0;
+    
+    cout << "os numeros pares são: " << endl;
+    
+    while (contador < 21){
+        cout << contador << endl;
+        contador+=2;
+    }
+    
+    cout << "Fim do Programa";
+    
+    return 0;
+}
