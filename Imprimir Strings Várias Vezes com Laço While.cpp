58aff5b95bef4aef02f7b2be0ddb12fed79ce0e8
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-17 10:48:34 -0300

    Imprimir Strings Várias Vezes com Laço While

---
+++ Imprimir Strings Várias Vezes com Laço While.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int cont = 0;
+    
+    while (cont < 20){
+        cout << "Inscrevam-se no canal do JHONY " << endl;
+        cont++;
+    }
+    
+    cout << "Fim do Programa";
+    
+    return 0;
+}
