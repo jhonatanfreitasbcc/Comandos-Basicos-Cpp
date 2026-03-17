58ca6c8fa68d39855a02fd2373e83e08933d8dc0
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-17 13:11:56 -0300

    Mostrar Números Ímpares com Laço For

---
+++ Mostrar Números Ímpares com Laço For.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int valor;
+    
+    for (valor = 1; valor < 21; valor+=2){
+        cout << valor << endl;
+    }
+    cout << "Programa Finalizado" << endl;
+    
+    return 0;
+}
