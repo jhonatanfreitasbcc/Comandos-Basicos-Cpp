f7e9538aad1563b7490e8c75ccf174b416f43f6d
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-03-27 10:42:25 -0300

    Como Pegar os Números Ímpares Dentro do Vetor

---
+++ Como Pegar os Números Ímpares Dentro do Vetor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+
+    std:: vector<int> vetor = {3, 1, 5, 2, 8, 9, 0};
+
+    std:: cout << "os numeros impares dentro do vetos sao: " << std:: endl;
+
+    for (int i = 0; i < vetor.size(); i++){
+        if (vetor[i] %2 != 0){
+            std:: cout << vetor[i] << std:: endl;
+        }
+    }
+
+    return 0;
+}
