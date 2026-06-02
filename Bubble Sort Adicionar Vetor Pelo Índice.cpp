27ba724143df88b76078dcf20d56023be0aeaa40
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-06-01 22:10:40 -0300

    Bubble Sort Adicionar Vetor Pelo Índice.cpp

---
+++ Bubble Sort Adicionar Vetor Pelo Índice.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    
+    std::vector<int> vetor;
+    int i;
+    int j;
+    int tamanho;
+    int valor;
+    int temp;
+    int indice;
+    
+    std:: cout << "Digite o tamanho do vetor: ";
+    std:: cin >> tamanho;
+    
+    for (i = 0; i < tamanho; i++){
+        std:: cout << "Digite os valores do vetor: ";
+        std:: cin >> valor;
+        vetor.push_back(valor);
+    }
+    
+    for (i = 0; i < vetor.size(); i++){
+        for (j = 0; j < vetor.size() - 1; j++){
+            if (vetor[j + 1] < vetor[j]){
+                temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        
+        }
+        
+    }
+    
+    std:: cout << "Digite o indíce onde quer adicionar o vetor: ";
+    std:: cin >> indice;
+    
+    vetor.insert(vetor.begin() + indice, 23);
+        
+    std:: cout << " os vetores ordenados sao: ";
+        
+    for (i = 0; i < vetor.size(); i++){
+        std:: cout << vetor[i] << " ";
+    }
+    
+    return 0;
+}
