24103df801616200109cf7f941cd4cdd9651891c
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-06-01 21:54:34 -0300

    Selection Sort Remover Vetor por Índice.cpp

---
+++ Selection Sort Remover Vetor por Índice.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    
+    std::vector<int> vetor;
+    int qtd;
+    int valor;
+    int i;
+    int j;
+    int menor;
+    int temp;
+    int indice;
+    
+    std:: cout << "digite o tamanho do vetor: ";
+    std:: cin >> qtd;
+    
+    for (i = 0; i < qtd; i++){
+        std:: cout << " digite os valores do vetor: ";
+        std:: cin >> valor;
+        vetor.push_back(valor);
+    }
+    
+    for (i = 0; i < qtd - 1; i++){
+        menor = i;
+        
+        for (j = i + 1; j < vetor.size(); j++){
+            if (vetor[j] < vetor[menor]){
+                menor = j;
+            }
+        }
+        
+        temp = vetor[i];
+        vetor[i] = vetor[menor];
+        vetor[menor] = temp;
+    }
+    
+    std:: cout << " digite o indice que deseja adicionar o vetor: ";
+    std:: cin >> indice;
+    
+    vetor.erase(vetor.begin() + indice);
+    std:: cout << " os vetores ordenados sao: ";
+    
+    for (i = 0; i < vetor.size(); i++){
+        std:: cout << vetor[i] << " ";
+    }
+    
+    return 0;
+}
