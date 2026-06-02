c48a828085faf382dc1c8de36105e0d0b45dc6c9
jhonatan freitas <jhonatanf@alunos.utfpr.edu.br>
2026-06-01 22:05:05 -0300

    Bubble Sort com Vetores Dinâmicos

---
+++ Bubble Sort com Vetores Dinâmicos.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+int main(){
+    
+    std::vector<int> vetor;
+    int i;
+    int j;
+    int tamanho;
+    int valor;
+    int temp;
+    
+    std:: cout << "Digite o tamanho do vetor: ";
+    std:: cin >> tamanho;
+    
+    for (i = 0; i < tamanho; i++){
+        std:: cout << "Digite os valores do vetor: ";
+        std:: cin >> valor;
+        vetor.push_back(valor);
+    }
+    
+    for (i = 0; i < vetor.size(); i++){
+        for (j = 0; j < vetor.size() - 1; j++){
+            if (vetor[j + 1] < vetor[j]){
+                temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
+            }
+        
+        }
+        
+    }
+        
+    std:: cout << " os vetores ordenados sao: ";
+        
+    for (i = 0; i < vetor.size(); i++){
+        std:: cout << vetor[i] << " ";
+    }
+    
+    return 0;
+}
